a62ba065eec1644bcc4236c7136a1edf1cc5aca9
Laura <laura93o@hotmail.com>
2025-09-27 16:54:43 +0200

    Update galeria.php

--- componentes/galeria.php
+++ componentes/galeria.php
@@ -3,29 +3,7 @@ include_once(__DIR__ . '/../config/db.php');
 
 $sql = "SELECT foto_url FROM entradas WHERE seccion_id = 2 AND foto_url IS NOT NULL AND TRIM(foto_url) <> '' ORDER BY id ASC";
 $result = $conn->query($sql);
-// if ($result && $result->num_rows > 0) {
-    
 
-//     // Mostrar la imagen grande inicial
-//     echo '<div class="visor">';
-//     // echo '<h2>Galería</h2>';
-//     $row = $result->fetch_assoc();
-//     $foto = htmlspecialchars($row["foto_url"]);
-//     echo '<img id="imagen-grande" src="/mi_pagweb/' . $foto . '" alt="Imagen grande">';
-//     echo '</div>';
-
-//     // Mostrar miniaturas
-//     echo '<div class="miniaturas">';
-//     echo '<img src="/mi_pagweb/' . $foto . '" onclick="mostrarImagen(this)">';
-//     while ($row = $result->fetch_assoc()) {
-//         $foto = htmlspecialchars($row["foto_url"]);
-//         echo '<img src="/mi_pagweb/' . $foto . '" onclick="mostrarImagen(this)">';
-//     }
-//     echo '</div>';
-
-// } else {
-//     echo '<p>No hay imágenes en la galería.</p>';
-// }
 
 if ($result && $result->num_rows > 0) {
 
