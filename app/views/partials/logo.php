f262edf772c0758ce104ef504a4f4ae4bea1a0f5
Laura Ordoñez <laura.ordonez.dev@gmail.com>
2026-09-08 20:52:06 +0200

    actualizacion logo cabecera

--- app/views/partials/logo.php
+++ app/views/partials/logo.php
@@ -4,70 +4,29 @@ $logo_url = !empty($header['logo']) ? asset_url($header['logo']) : '';
 $logo_url = htmlspecialchars($logo_url, ENT_QUOTES, 'UTF-8');
 ?>
 <style>
+   header#inicio {
+    background-image: url('<?php echo $logo_url; ?>');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-color: #000;
+    width: 100%;
+    min-height: 900px; /* Ajusta según necesites */
+    max-height: none; /* Elimina el max-height */
+    display: flex;
+    justify-content: center;
+    position: relative;
+}
+
+@media screen and (max-width: 768px) {
     header#inicio {
-        background-image: url('<?php echo $logo_url; ?>');
-        background-size: cover;
-        background-position: top center;
-        background-repeat: no-repeat;
-        width: 100%;
-        min-height: 99vh;
-        display: flex;
-        justify-content: center;
-        position: relative;
-        /* border-bottom: 2px solid #4B1E78; */
+        min-height: 400px;
     }
+}
 
-    @media screen and (max-width: 1440px) {
-        header#inicio {
-            background-image: url('<?= asset_url('img/cabecera.webp') ?>');
-            min-height: 95vh;
-            padding: 2rem;
-            background-size: cover;
-            background-position: center top;
-            background-attachment: scroll;
-        }
+@media screen and (max-width: 480px) {
+    header#inicio {
+        min-height: 300px;
     }
-
-    /* Portátiles pequeños tipo 14" */
-    @media screen and (min-width: 1200px) and (max-width: 1450px) and (max-height: 850px) {
-        header#inicio {
-            min-height: 100vh;
-            padding: 0;
-            box-sizing: border-box;
-            background-size: contain;
-            background-color: #000;
-            background-position: center top;
-            background-repeat: no-repeat;
-        }
-    }
-
-    @media screen and (max-width: 1024px) {
-        header#inicio {
-            background-image: url('<?= asset_url('img/cabecera.webp') ?>');
-            min-height: 53vh;
-            padding: 1.5rem;
-            background-position: center center;
-            background-size: cover;
-        }
-    }
-
-    @media screen and (max-width: 768px) {
-        header#inicio {
-            min-height: 47vh;
-            padding: 2rem;
-            background-position: center center;
-            background-attachment: scroll;
-        }
-    }
-
-    @media screen and (max-width: 480px) {
-        header#inicio {
-            background-image: url('<?= asset_url('img/cabecera.webp') ?>');
-            min-height: 30vh;
-            padding: 1.2rem;
-            background-position: right center;
-            background-size: cover;
-            -webkit-text-size-adjust: 100%;
-        }
-    }
+}
 </style>
